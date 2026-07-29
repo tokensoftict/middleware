65438bccc9986a5890105a3124d7602ad744a0e9
Yusuf Olatunji <yoyforever@gmail.com>
2026-07-29 10:22:13 +0100

    logging webhook

--- app/Http/Controllers/Api/WebhookController.php
+++ app/Http/Controllers/Api/WebhookController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Actions\WebhookProxyController;
 use App\Http\Controllers\Controller;
 use App\Services\Webhook\WebhookService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
@@ -16,32 +16,32 @@ class WebhookController extends Controller
 
     public function handle(Request $request, string $serviceSlug, string $service_uuid, ?string $eventType = null): JsonResponse
     {
-        WebhookProxyController::forward($request);
+        Log::info($request->getContent());
 
         return response()->json(['status' => 'ok']);
 
-//        try {
-//            $this->webhookService->receive($request, $serviceSlug, $service_uuid, $eventType);
-//
-//            return response()->json(['status' => 'ok']);
-//        } catch (Exception $e) {
-//            $code = $e->getCode();
-//
-//            if ($code === 400) {
-//                return response()->json(['status' => 'error', 'message' => 'Invalid signature'], 400);
-//            }
-//
-//            if (in_array($code, [404, 503], true)) {
-//                return response()->json(['status' => 'error', 'message' => $e->getMessage()], $code);
-//            }
-//
-//            Log::error('Webhook processing error', [
-//                'service' => $serviceSlug,
-//                'event_type' => $eventType,
-//                'error' => $e->getMessage(),
-//            ]);
-//
-//            return response()->json(['status' => 'ok']);
-//        }
+        //        try {
+        //            $this->webhookService->receive($request, $serviceSlug, $service_uuid, $eventType);
+        //
+        //            return response()->json(['status' => 'ok']);
+        //        } catch (Exception $e) {
+        //            $code = $e->getCode();
+        //
+        //            if ($code === 400) {
+        //                return response()->json(['status' => 'error', 'message' => 'Invalid signature'], 400);
+        //            }
+        //
+        //            if (in_array($code, [404, 503], true)) {
+        //                return response()->json(['status' => 'error', 'message' => $e->getMessage()], $code);
+        //            }
+        //
+        //            Log::error('Webhook processing error', [
+        //                'service' => $serviceSlug,
+        //                'event_type' => $eventType,
+        //                'error' => $e->getMessage(),
+        //            ]);
+        //
+        //            return response()->json(['status' => 'ok']);
+        //        }
     }
 }
